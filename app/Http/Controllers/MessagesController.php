147735b3333fdb4f320e65862df143e7e4dcf258
EC2 Default User <ec2-user@ip-172-31-26-77.us-west-2.compute.internal>
2018-04-27 01:57:17 +0000

    CRUD messages

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Message; // 追加
+
+class MessagesController extends Controller
+{
+    /**
+     * getでmessages/にアクセスされた場合の「一覧表示処理」
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $messages = Message::all();
+
+        return view('messages.index', [
+            'messages' => $messages,
+        ]);
+    }
+
+    /**
+     * getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $message = new Message;
+
+        return view('messages.create', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * postでmessages/にアクセスされた場合の「新規登録処理」
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $message = new Message;
+        $message->content = $request->content;
+        $message->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * getでmessages/idにアクセスされた場合の「取得表示処理」
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $message = Message::find($id);
+
+        return view('messages.show', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * getでmessages/id/editにアクセスされた場合の「更新画面表示処理」
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $message = Message::find($id);
+
+        return view('messages.edit', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $message = Message::find($id);
+        $message->content = $request->content;
+        $message->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * deleteでmessages/idにアクセスされた場合の「削除処理」
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $message = Message::find($id);
+        $message->delete();
+
+        return redirect('/');
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,26 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('/', 'MessagesController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('messages', 'MessagesController');
+
+
+// // CRUD
+// // メッセージの個別詳細ページ表示
+// Route::get('messages/{id}', 'MessagesController@show');
+// // メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+// Route::post('messages', 'MessagesController@store');
+// // メッセージの更新処理（編集画面を表示するためのものではありません）
+// Route::put('messages/{id}', 'MessagesController@update');
+// // メッセージを削除
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+
+// // index: showの補助ページ
+// Route::get('messages', 'MessagesController@index');
+
+// // create: 新規作成用のフォームページ
+// Route::get('messages/create', 'MessagesController@create');
+
+// // edit: 更新用のフォームページ
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
